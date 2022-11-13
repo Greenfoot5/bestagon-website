456be4a2c6af8f6309d8ec6fc02f20393491f765
Greenfoot5 <greenfoot89@gmail.com>
2022-11-13 17:37:01 +0000

    Setup the base site for Bestagon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "*.{html,js}"
+      "*.{html,js}",
   ],
   theme: {
     extend: {
-      // ...
-    },
+      colors: {
+        bestagon: {
+          background: '#1e202c',
+          // Turrets
+          smasher: '#e11E5e',
+          shooter: '#1FCF89',
+          laser: '#22C1D7',
+          lancer: '#3444AB',
+          gunner: '#9638F2',
+        }
+      },
+      fontFamily: {
+        'futured': ['futured'],
+      }
+    }
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
