cbd8be5c346f4e0defaf56976a26421ec1736784
Greenfoot5 <greenfoot89@gmail.com>
2022-11-30 21:51:03 +0000

    Expanded the site colours to include a dark and light colour too

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,41 @@ module.exports = {
         bestagon: {
           background: '#1e202c',
           // Turrets
-          smasher: '#e11E5e',
-          shooter: '#1FCF89',
-          laser: '#22C1D7',
-          lancer: '#3444AB',
-          gunner: '#9638F2',
+          any: {
+            DEFAULT: '#989898',
+            light: '#cecece',
+            dark: '#737373',
+          },
+          smasher: {
+            DEFAULT: '#df1d5c',
+            light: '#f02d8f',
+            dark: '#a71545',
+          },
+          shooter: {
+            DEFAULT: '#1ecd88',
+            light: '#30e5c6',
+            dark: '#179b67',
+          },
+          laser: {
+            DEFAULT: '#21bfd8',
+            light: '#30e1e9',
+            dark: '#1790a0',
+          },
+          lancer: {
+            DEFAULT: '#4857e0',
+            light: '#6987ed',
+            dark: '#3342aa',
+          },
+          gunner: {
+            DEFAULT: '#9c36f8',
+            light: '#ce56fd',
+            dark: '#742bba',
+          },
+          choker: {
+            DEFAULT: '#f8653a',
+            light: '#f8653a',
+            dark: '#bb4c29',
+          }
         }
       },
       fontFamily: {
